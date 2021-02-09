414988a27eb3a2f6f056db77e5b9c8e21cebb078
haseeb129 <haseeb129ciit@gmail.com>
2021-02-09 17:17:18 +0500

    woking

--- src/stories/Page3.js
+++ src/stories/Page3.js
@@ -3,6 +3,10 @@ import "./Page3.css";
 // FaArrowsAltH
 import { FaArrowsAltH } from "react-icons/fa";
 
+import { Row, Col } from "react-bootstrap";
+
+import image3 from "../images/efd31d99578da3e3d070b1038625a273fb769a0c.png";
+
 export const Page3 = () => (
   <div className="pageSize3 main3">
     <br />
@@ -48,6 +52,38 @@ export const Page3 = () => (
           <FaArrowsAltH />
         </span>
       </div>
+    </div>
+
+    <div className="inner3">
+      <span style={{ fontSize: "24px" }}>Blends Well With Others</span>
+      <br />
+      <Row style={{ marginTop: "20px" }}>
+        <Col xs={6} sm={6}>
+          <img src={image3} className="roundImage" />
+          <span style={{ fontSize: "20px" }}>Lavender</span>
+          <div className="afterImageButton">View</div>
+        </Col>
+        <Col xs={6} sm={6}>
+          <img src={image3} className="roundImage" />
+          <span style={{ fontSize: "20px" }}>Lavender</span>
+
+          <div className="afterImageButton">View</div>
+        </Col>
+      </Row>
+      <Row style={{ marginTop: "20px", marginBottom: "50px" }}>
+        <Col xs={6} sm={6}>
+          <img src={image3} className="roundImage" />
+          <span style={{ fontSize: "20px" }}>Lavender</span>
+
+          <div className="afterImageButton">View</div>
+        </Col>
+        <Col xs={6} sm={6}>
+          <img src={image3} className="roundImage" />
+          <span style={{ fontSize: "20px" }}>Lavender</span>
+
+          <div className="afterImageButton">View</div>
+        </Col>
+      </Row>
     </div>
   </div>
 );
